a0cc9a8fb96f92f0d42e2247e57ce625b3e41fd3
Panagioti (Pete) Tsivis <87956455+Greekman12490@users.noreply.github.com>
2022-01-02 20:31:50 -0500

    Add files via upload

--- sql/diabetes.sql
+++ sql/diabetes.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE Diabetes;
 
 CREATE TABLE diabetes_health_indicators (
+	Client_id int,
     Diabetes_012 float,
 	HighBP float,
     HighChol float,
@@ -23,11 +24,17 @@ CREATE TABLE diabetes_health_indicators (
 	Age float,
 	Education float,
 	Income float
-);
-
-COPY diabetes_012_health_indicators_BRFSS2015(Diabetes_012, HighBP, HighChol, CholCheck, BMI, Smoker, Stroke, HeartDiseaseorAttack, PhysActivity,
+); 
+COPY diabetes_012_health_indicators_BRFSS2015(Client_id, Diabetes_012, HighBP, HighChol, CholCheck, BMI, Smoker, Stroke, HeartDiseaseorAttack, PhysActivity,
 	Fruits, Veggies, HvyAlcoholConsump, AnyHealthcare, NoDocbcCost, GenHlth, MentHlth, PhysHlth, DiffWalk, Sex, Age, Education, Income)
 FROM 'C:\Users\Pete\OneDrive\Desktop\Rutgers Certification Classwork\Final Project\csv\diabetes_012_health_indicators_BRFSS2015.csv'
 DELIMITER ','
 CSV HEADER;
+
+CREATE TABLE lifestyle AS (SELECT client_id, diabetes_012, cholcheck, smoker, physactivity, fruits, veggies, hvyalcoholconsump from diabetes_health_indicators);
+
+CREATE TABLE socioeconomic AS (SELECT client_id, diabetes_012, anyhealthcare, nodocbccost, sex, age, education, income from diabetes_health_indicators);
+
+CREATE TABLE health_indicators AS (SELECT highbp, highchol, bmi, stroke, heartdiseaseorattack, diffwalk from diabetes_health_indicators);
+
 
