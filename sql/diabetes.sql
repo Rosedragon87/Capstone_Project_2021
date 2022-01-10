76f8d953dc8266670ee53bc8dda398f25b172ac5
Panagioti (Pete) Tsivis <87956455+Greekman12490@users.noreply.github.com>
2022-01-09 22:36:12 -0500

    Add files via upload

--- sql/diabetes.sql
+++ sql/diabetes.sql
@@ -54,12 +54,12 @@ CREATE TABLE lifestyle_nonpre AS (SELECT client_id, diabetes_012, cholcheck, smo
 
 CREATE TABLE socioeconomic_nonpre AS (SELECT client_id, diabetes_012, anyhealthcare, nodocbccost, sex, age, education, income from nondiabetes_and_diabetes_health_indicators);
 
-CREATE TABLE health_indicators_nonpre AS (SELECT highbp, highchol, bmi, stroke, heartdiseaseorattack, diffwalk from nondiabetes_and_diabetes_health_indicators);
+CREATE TABLE health_indicators_nonpre AS (SELECT client_id, diabetes_012, highbp, highchol, bmi, stroke, heartdiseaseorattack, diffwalk from nondiabetes_and_diabetes_health_indicators);
 
 CREATE TABLE lifestyle_nondiabetes AS (SELECT client_id, diabetes_012, cholcheck, smoker, physactivity, fruits, veggies, hvyalcoholconsump from nondiabetes_and_prediabetes_health_indicators);
 
 CREATE TABLE socioeconomic_nondiabetes AS (SELECT client_id, diabetes_012, anyhealthcare, nodocbccost, sex, age, education, income from nondiabetes_and_prediabetes_health_indicators);
 
-CREATE TABLE health_indicators_nondiabetes AS (SELECT highbp, highchol, bmi, stroke, heartdiseaseorattack, diffwalk from nondiabetes_and_prediabetes_health_indicators);
+CREATE TABLE health_indicators_nondiabetes AS (SELECT client_id, diabetes_012, highbp, highchol, bmi, stroke, heartdiseaseorattack, diffwalk from nondiabetes_and_prediabetes_health_indicators);
 
 
